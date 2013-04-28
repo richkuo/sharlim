e6820c37b96ebc85c04cced47a229d462abf834b
richardkuo <richard.p.kuo@gmail.com>
2013-04-28 14:43:09 -0400

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def help
+  end
+
+  def contact
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharlim::Application.config.secret_token = '7b574769e39c54a4d56595b773f6fa51e3b3ba6ac684966596f55e6bde33cca578235f12b9b9467340c2652ec5b3801c2c451c03b2955867ce678f9fe118b4b8'
