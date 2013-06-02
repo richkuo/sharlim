85eda22e70a1962e25a1fb8461ae74fe32169071
richkuo <richard.p.kuo@gmail.com>
2013-06-02 00:36:47 -0400

    redirect after sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   	raise ActionController::RoutingError.new('Not Found')
 	end
 	
+	def after_sign_in_path_for(resource)
+		root_path
+	end
+
 end
